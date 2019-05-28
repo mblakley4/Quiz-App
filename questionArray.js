8d75af0388a9a55e03ffafe077deee154aa1e3be
Mike Blakley <m.blakley@icloud.com>
2019-05-28 17:28:53 -0400

    added questionArray.js

---
+++ questionArray.js
@@ -0,0 +1,242 @@
+const STORE = [
+    //1st question
+    {
+        question: 'One of the most common compound exercises, the Squat, targets which muscle group(s)?',
+            choice1OBJ: {
+                choice1:'quadriceps',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice2OBJ: {
+                choice2:'gluteus maximus',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice3OBJ: {
+                choice3:'hamstrings',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice4OBJ: {
+                choice4:'all of the above',
+                correctChoice: true,
+                userAnswer: false,
+            },
+    },
+    //2nd question
+    {
+        question: 'Development and growth of muscle through strength and resistance training is referred to as?',
+            choice1OBJ: {
+                choice1:'atrophy',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice2OBJ: {
+                choice2:'hypertrophy',
+                correctChoice: true,
+                userAnswer: false,
+            },
+            choice3OBJ: {
+                choice3:'metaplasia',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice4OBJ: {
+                choice4:"'getting swoll'",
+                correctChoice: false,
+                userAnswer: false,
+            },
+    },
+    //3rd question
+    {
+        question: 'Abdominal muscles can be targeted through which of the following exercises?',
+            choice1OBJ: {
+                choice1:'scull crushers',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice2OBJ: {
+                choice2:'romanian deadlifts',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice3OBJ: {
+                choice3:'hanging leg raises',
+                correctChoice: true,
+                userAnswer: false,
+            },
+            choice4OBJ: {
+                choice4:'barbell rows',
+                correctChoice: false,
+                userAnswer: false,
+            },
+    },
+    //4th question
+    {
+        question: 'All of the following are great back exercises, except?',
+            choice1OBJ: {
+                choice1:'close grip bench press',
+                correctChoice: true,
+                userAnswer: false,
+            },
+            choice2OBJ: {
+                choice2:'bent-over barbell rows',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice3OBJ: {
+                choice3:'pull-ups',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice4OBJ: {
+                choice4:'deadlifts',
+                correctChoice: false,
+                userAnswer: false,
+            },
+    },
+    //5th question
+    {
+        question: 'You can target your anterior, medial, and posterior deltoids with which three exercises?',
+            choice1OBJ: {
+                choice1:'incline, flat, and decline flyes',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice2OBJ: {
+                choice2:'body weight squat to pushup to jump',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice3OBJ: {
+                choice3:'front raises, lateral raises, and rear flyes',
+                correctChoice: true,
+                userAnswer: false,
+            },
+            choice4OBJ: {
+                choice4:'kettle bell swings, turkish get-ups, and box jumps',
+                correctChoice: false,
+                userAnswer: false,
+            },
+    },
+    //6th question
+    {
+        question: 'All of the following are types of bicep curls, except?',
+            choice1OBJ: {
+                choice1:'preacher',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice2OBJ: {
+                choice2:'hammer',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice3OBJ: {
+                choice3:'concentration',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice4OBJ: {
+                choice4:'sumo',
+                correctChoice: true,
+                userAnswer: false,
+            },
+    },
+    //7th question
+    {
+        question: 'A common way to group upper body exercises is termed?',
+            choice1OBJ: {
+                choice1:'twist / bend',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice2OBJ: {
+                choice2:'push / pull',
+                correctChoice: true,
+                userAnswer: false,
+            },
+            choice3OBJ: {
+                choice3:'to / fro',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice4OBJ: {
+                choice4:'out / in',
+                correctChoice: false,
+                userAnswer: false,
+            },
+    },
+    //8th question
+    {
+        question: 'What is it called when you string together more than one exercise with minimal to no rest?',
+            choice1OBJ: {
+                choice1:'superset',
+                correctChoice: true,
+                userAnswer: false,
+            },
+            choice2OBJ: {
+                choice2:'playset',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice3OBJ: {
+                choice3:'Thor-set',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice4OBJ: {
+                choice4:'dropset',
+                correctChoice: false,
+                userAnswer: false,
+            },
+    },
+    //9th question
+    {
+        question: 'Romanian deadlifts, leg curls, lunges and bulgarian split squats can all help strengthen which muscle?',
+            choice1OBJ: {
+                choice1:'calves',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice2OBJ: {
+                choice2:'hamstrings',
+                correctChoice: true,
+                userAnswer: false,
+            },
+            choice3OBJ: {
+                choice3:'diaphram',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice4OBJ: {
+                choice4:'pectorals',
+                correctChoice: false,
+                userAnswer: false,
+            },
+    },
+    //10th question
+    {
+        question: 'When lifting weights, it is important to prioritize ________ over __________.',
+            choice1OBJ: {
+                choice1:'mirror muscles; leg day',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice2OBJ: {
+                choice2:'calves; forearms',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice3OBJ: {
+                choice3:'hard rock; country music',
+                correctChoice: false,
+                userAnswer: false,
+            },
+            choice4OBJ: {
+                choice4:'proper form; intensity',
+                correctChoice: false,
+                userAnswer: false,
+            },
+    }
+]
